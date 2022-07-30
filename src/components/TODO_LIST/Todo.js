9178b55bc33ba872ec34abdbb37ffc0dac391864
Ankit Shrestha <ankit.shrestha196@gmail.com>
2022-07-30 21:09:17 +0545

    Initial Commit

---
+++ src/components/TODO_LIST/Todo.js
@@ -0,0 +1,12 @@
+import Card from "../UI/Card"
+import "./Todo.css"
+
+const Todo = () => {
+  return (
+    <Card className="todo-list">
+      <div className="todo-list__item"></div>
+    </Card>
+  )
+}
+
+export default Todo
